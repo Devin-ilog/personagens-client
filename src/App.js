5f5d37de778119f74e648ab670078130c3ba1990
tiagoamp <tiagoamp@gmail.com>
2023-04-27 20:00:42 -0300

    Implementa acesso à API REST de personagens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ListaPersonagens from './components/ListaPersonagens';
 import './App.css';
 
 function App() {
+
+  const [personagens, setPersonagens] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/personagens')
+      .then(resp => setPersonagens(resp.data))
+      .catch(erro => console.log(erro));
+  }, [])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de Personagens cadastrados</h1>
+      
+      <ListaPersonagens personagens={personagens} />
+
     </div>
+  
   );
 }
 
